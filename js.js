7d65bcb3030c31571337313e4848e14368b22ed3
amalyakubov <129682707+amalyakubov@users.noreply.github.com>
2023-04-28 16:38:28 +0200

    Make the display interactive
    Create a function 'operate()' that uses the arguments 'a', 'b', 'c'
    in order to run a function requested by the user
    If argument 'b' is a symbol that represents a mathematical operation,
    run the corresponding function.
    
    Add the user interface with buttons and a div that displays the result
    in order for the user to use the buttons instead of typing out the
    function in the console
    
    Make numbers appear on the display when the user clicks the numbered
    buttons using an eventListener and a variable that stores the value
    of a clicked button
    
    Add the class 'number' to the numbers with the numeric value to select
    them as a value for the 'buttons' variable.

--- js.js
+++ js.js
@@ -13,3 +13,33 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(firstNumber, operator, secondNumber) {
+    if (operator === '+') {
+        add(firstNumber, secondNumber);
+    } else if (operator === '-') {
+        subtract(firstNumber, secondNumber);
+    } else if (operator === '*') {
+        multiply(firstNumber, secondNumber);
+    } else if (operator === '/') {
+        divide(firstNumber, secondNumber);
+    } else {
+        return 'Unknown error';
+    }
+}
+
+let displayValue;
+let buttons = document.body.getElementsByClassName('number');
+
+Array.from(buttons).forEach(element => {
+    addEventListener('click', (event) => {
+        displayValue = event.target.textContent;
+        display.textContent = displayValue;
+    })
+});
+
+let display = document.querySelector('div');
