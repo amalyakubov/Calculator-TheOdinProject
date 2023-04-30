0957d9ec1cde4de762a6a1522cc2fc611a03e870
amalyakubov <129682707+amalyakubov@users.noreply.github.com>
2023-04-30 11:32:23 +0200

    Add 'clear' functionality improved logic
    Add the clear functionality and add the ability to calculate
    multi-digit numbers by changing how the input changes the firstNumber
    variable

--- js.js
+++ js.js
@@ -3,43 +3,95 @@ function add(a, b) {
 } 
 
 function subtract(a, b) {
-    return a - b;
+   return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
+   return a * b;
 }
 
 function divide(a, b) {
-    return a / b;
+   return a / b;
 }
 
-let firstNumber;
+let firstNumber = [];
 let operator;
-let secondNumber;
+let secondNumber = [];
 
 function operate(firstNumber, operator, secondNumber) {
     if (operator === '+') {
-        add(firstNumber, secondNumber);
+      return add(+firstNumber.join(''), +secondNumber.join(''));
     } else if (operator === '-') {
-        subtract(firstNumber, secondNumber);
+      return subtract(+firstNumber.join(''), +secondNumber.join(''));
     } else if (operator === '*') {
-        multiply(firstNumber, secondNumber);
+      return multiply(+firstNumber.join(''),+secondNumber.join(''));
     } else if (operator === '/') {
-        divide(firstNumber, secondNumber);
+      return divide(+firstNumber.join(''), +secondNumber.join(''));
     } else {
-        return 'Unknown error';
+      return 'Unknown error';
     }
 }
 
+let display = document.querySelector('div');
 let displayValue;
 let buttons = document.body.getElementsByClassName('number');
+let operations = document.body.getElementsByClassName('operations');
 
-Array.from(buttons).forEach(element => {
-    addEventListener('click', (event) => {
+Array.from(buttons).forEach(function(elem) {
+    elem.addEventListener('click', (event) => {
+        event.stopPropagation();
+        if (operatorClicked === false) {
+            firstNumber.push(+event.target.textContent);
+        } else if (operator != 0 || operator != undefined) {
+            secondNumber.push(+event.target.textContent);
+        }
         displayValue = event.target.textContent;
         display.textContent = displayValue;
     })
 });
 
-let display = document.querySelector('div');
+let operatorClicked = false;
+
+let calc = document.getElementById('calc');
+calc.addEventListener('click', (event) => {
+    event.stopPropagation();
+    displayValue = operate(firstNumber, operator, secondNumber);
+    display.textContent = displayValue;
+})
+
+let addition = document.getElementById('addition');
+addition.addEventListener('click', (event) => {
+    event.stopPropagation();
+    operator = '+';
+    operatorClicked = true;
+})
+
+let subtraction = document.getElementById('subtraction');
+subtraction.addEventListener('click', (event) => {
+    event.stopPropagation();
+    operator = '-';
+    operatorClicked = true;
+})
+
+let multiplication = document.getElementById('multiplication');
+multiplication.addEventListener('click', (event => {
+    event.stopPropagation();
+    operator = '*';
+    operatorClicked = true;
+}))
+
+let division = document.getElementById('division');
+division.addEventListener('click', (event) => {
+    event.stopPropagation();
+    operator = '/';
+    operatorClicked = true;
+})
+
+let clear = document.getElementById('clear');
+clear.addEventListener('click', (event) => {
+    event.stopPropagation();
+    firstNumber = 0;
+    operator = 0;
+    secondNumber = 0;
+    display.textContent = '';
+})
